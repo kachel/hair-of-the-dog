840818cb74ca4791cc6c0385826e947aa3cd495b
Kachel Wittig <kjwittig@gmail.com>
2018-12-10 21:32:18 -0600

    create recipe ingredient and comment models with AR relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes
-  has_many :posts, through: :recipes
+  has_many :comments, through: :recipes
+  has_many :ingredients, through: :recipes
   has_secure_password
 end
