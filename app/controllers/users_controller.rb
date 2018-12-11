9eeb4d9bc4da74c6cbf2b29f48a4a4de3e8ec818
Kachel Wittig <kjwittig@gmail.com>
2018-12-10 22:20:37 -0600

    add welcome route and view for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def welcome
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
 end
