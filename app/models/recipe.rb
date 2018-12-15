bfed3bd02ebc12643392da712665e194fa636d2a
Kachel Wittig <kjwittig@gmail.com>
2018-12-14 20:10:37 -0600

    Change has many through relationship for recipes and users

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :comments, through: :users
-  has_many :ratings, through: :users
+  has_many :comments
+  has_many :commenting_users, through: :comments, source: :users
+  has_many :rating_users, through: :ratings, source: :users
   has_many :ingredients
   has_many :directions
   accepts_nested_attributes_for :directions
   accepts_nested_attributes_for :ingredients
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_many :recipes
-  has_many :comments, through: :recipes
-  has_many :ratings, through: :recipes
+  has_many :commented_recipes, through: :comments, source: :recipes
+  has_many :rated_recipes, through: :ratings, source: :recipes
   has_secure_password
 
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :password, presence: true
 
-  # inspired by Devise User#from_omniauth 
+  # inspired by Devise User#from_omniauth
   def self.find_or_create_by_omniauth(auth_hash)
     where(email: auth_hash["info"]["email"]).first_or_create do |u|
       u.password = SecureRandom.hex
