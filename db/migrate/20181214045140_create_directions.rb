9ef4f75b920e6e2ad923004a082b42786192cc9a
Kachel Wittig <kjwittig@gmail.com>
2018-12-14 19:09:14 -0600

    Modify direction table from steps to body

--- db/migrate/20181214045140_create_directions.rb
+++ db/migrate/20181214045140_create_directions.rb
@@ -2,11 +2,7 @@ class CreateDirections < ActiveRecord::Migration[5.2]
   def change
     create_table :directions do |t|
       t.integer :recipe_id
-      t.text :step_1
-      t.text :step_2
-      t.text :step_3
-      t.text :step_4
-      t.text :step_5
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_041426) do
+ActiveRecord::Schema.define(version: 2018_12_14_045140) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -22,11 +22,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_041426) do
 
   create_table "directions", force: :cascade do |t|
     t.integer "recipe_id"
-    t.text "step_1"
-    t.text "step_2"
-    t.text "step_3"
-    t.text "step_4"
-    t.text "step_5"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
