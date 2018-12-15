88374eb81cd0027b84050b76e9ad1580a73163ab
Kachel Wittig <kjwittig@gmail.com>
2018-12-14 21:37:57 -0600

    Build sign up form for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,22 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to @user
+    else
+      render new_user_url
+    end
   end
 
   def welcome
+    current_user
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:email, :password)
     end
 
 end
