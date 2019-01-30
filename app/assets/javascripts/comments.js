fad98dec7e733c491bc72d8042780470770cd791
Kachel Wittig <kjwittig@gmail.com>
2019-01-30 11:07:08 -0600

    Add js class constructor for comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,11 +1,22 @@
-class HOTDComment {
-  // To create a JS comment object call this constructor
-  //
-  //     new HOTDComment({id: 123, content: "hello", user: "florb"})
+~function () {
 
-  constructor(obj) {
-    this.id = obj.id
-    this.content = obj.content
-    this.email = obj.email
+  const allComments = []
+
+  class HOTDComment {
+    // To create a JS comment object call this constructor
+    //
+    //     new HOTDComment({id: 123, content: "hello", user: "florb"})
+
+    constructor(obj) {
+      this.id = obj.id
+      this.content = obj.content
+      this.email = obj.email
+    }
+
+    insertInto($container) {
+
+      $container.append(`<li> ${this.email} says: ${this.content} </li>`)
+    }
   }
+  
 }
