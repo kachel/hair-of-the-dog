886cd92bbdc5719f5499f1291ae1e4a5dadcec49
Kachel Wittig <kjwittig@gmail.com>
2019-01-30 07:08:03 -0600

    Serialize json and map to JS class object

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,2 +1,11 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+class HOTDComment {
+  // To create a JS comment object call this constructor
+  //
+  //     new HOTDComment({id: 123, content: "hello", user: "florb"})
+
+  constructor(obj) {
+    this.id = obj.id
+    this.content = obj.content
+    this.email = obj.email
+  }
+}
--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -18,9 +18,8 @@ $(function () {
 
     // high level JSON response
     $.get(this.href).done(function (json) {
-      json.forEach(function (comment) {
-        // create comment object
-      })
+      // destructured assignment + punning
+      json.map( {id, content, user} => new HOTDComment({id, content, email: user.email}) )
     })
   })
 })
