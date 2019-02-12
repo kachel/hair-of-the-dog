981be839fafe1486b3edcfd97270a9fd4ee49901
Kachel Wittig <kjwittig@gmail.com>
2019-02-12 14:05:04 -0600

    Fully implement previous recipe link with global function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,23 +1,32 @@
-$(function() {
-  $('.js-prev').on('click', function(e) {
-    e.preventDefault()
-    const userId = parseInt($('h2').attr('data-user-id'))
-    const recipeId = parseInt($('h3').attr('data-recipe-id'))
-    $.get('/users/' + userId + '.json', function(resp) {
-      const recipes = resp.recipes
+// purposeful root level function declaration
 
-      const recipeIds = recipes.map(recipe => recipe.id)
-      const prevRecipeIndex = (recipeIds.indexOf(recipeId)) - 1
+function prevRecipeClick() {
+  const userId = parseInt($('h2').attr('data-user-id'))
+  const recipeId = parseInt($('h3').attr('data-recipe-id'))
+  $.get('/users/' + userId + '.json', function(resp) {
+    const recipes = resp.recipes
+
+    const recipeIds = recipes.map(recipe => recipe.id)
+
+    const prevRecipeIndex = recipeIds.indexOf(recipeId) - 1
+
+    if (prevRecipeIndex >= 0) {
       const prevRecipe = recipes[prevRecipeIndex]
 
-      $('recipeTitle').text(prevRecipe['title'])
-      $('recipeServings').text(prevRecipe['servings'])
+      $('.recipeTitle').text(prevRecipe['title'])
+      $('.recipeServings').text('(serves ' + prevRecipe['servings'] + ')')
       $('ul').empty()
-      prevRecipe.ingredients.map(ingredient => $( "ul" ).append( "<li>" + ingredient.name + "</li>" ))
+      prevRecipe.ingredients.map(ingredient =>
+        $('ul').append('<li>' + ingredient.name + '</li>'),
+      )
       $('recipeDescription').text(prevRecipe['description'])
       $('ol').empty()
-      prevRecipe.directions.map(direction => $( "ol" ).append( "<li>" + direction.body + "</li>" ))
-      $('h3').attr("data-recipe-id", prevRecipe["id"]);
-    })
+      prevRecipe.directions.map(direction =>
+        $('ol').append('<li>' + direction.body + '</li>'),
+      )
+      $('h3').attr('data-recipe-id', prevRecipe['id'])
+    } else {
+      window.alert('No more recipes')
+    }
   })
-})
+}
